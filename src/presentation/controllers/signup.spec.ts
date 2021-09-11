0a1850fec473b4464dad9f66863400ce69660678
Felipe <felipe1180@hotmail.com>
2021-09-11 12:47:06 -0300

    test: ✅ ensure SinUpController return errors if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,17 +1,18 @@
-import { SignUpController } from './signup.controller';
+import { SignUpController } from './signup.controller'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController();
+    const sut = new SignUpController()
     const httpRequest = {
       body: {
         // name: 'Felipe Austríaco',
         email: 'felipe1180@hotmail.com',
         password: 'testpassword',
-        passwordConfirmation: 'testpassword',
-      },
-    };
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-  });
-});
+        passwordConfirmation: 'testpassword'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+})
