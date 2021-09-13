b7bce88161338f5fbe6868a9bdc10d2af79ca70c
Felipe <felipe1180@hotmail.com>
2021-09-13 16:13:51 -0300

    refactor: :recycle: refactoring success to a http-helper

--- src/presentation/controllers/signup/signup.controller.ts
+++ src/presentation/controllers/signup/signup.controller.ts
@@ -1,4 +1,4 @@
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { Controller, HttpRequest, HttpResponse, EmailValidator, AddAccount } from './signup-protocols'
 export class SignUpController implements Controller {
@@ -37,10 +37,7 @@ export class SignUpController implements Controller {
         password
       })
 
-      return {
-        statusCode: 200,
-        body: account
-      }
+      return ok(account)
     } catch (error) {
       return serverError()
     }
